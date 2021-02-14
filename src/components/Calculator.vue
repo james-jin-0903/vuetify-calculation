9ee20eea5254b85ce3e90900e2347d2bdcf0139c
unknown <jenny970903@outlook.com>
2021-02-15 02:14:46 +0800

    change style and formula

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,14 +2,15 @@
   <v-container>
     <v-row class="text-left justify-center">
       <div class="width-600">
-        <h3>
+        <h1>
           Calculator
-        </h3>
+        </h1>
         <p>
-          Default values do not imply return expectations. Change them up as necessary.
+          Default values do not imply return expectations. Change them up as
+          necessary.
         </p>
         <v-row>
-          <v-col cols="7">
+          <v-col cols="6">
             <v-row>
               <v-col cols="12" md="6" sm="6" xs="12" class="mobile-px-0">
                 <vuetify-money
@@ -50,10 +51,10 @@
                 </div>
               </v-col>
             </v-row>
-            <v-row class="show-desktop">
+            <v-row>
               <v-col cols="12">
                 <v-row>
-                  <v-col cols="6" md="6" sm="6" xs="6">
+                  <v-col cols="12">
                     <vuetify-money
                       v-model="tradingFeeCollected"
                       class="input-color-green"
@@ -63,92 +64,114 @@
                       :readonly="true"
                     />
                   </v-col>
-                  <v-col cols="6" md="6" sm="6" xs="6" class="d-flex align-center">
-                    <v-btn
-                      color="success"
-                    >
-                      Sign Up Now
-                    </v-btn>
-                  </v-col>
                 </v-row>
               </v-col>
             </v-row>
             <v-row class="show-desktop">
-              <v-col cols="12">
+              <v-col
+                cols="6"
+                md="6"
+                sm="6"
+                xs="6"
+                class="d-flex align-center"
+              >
                 <v-row>
                   <v-col cols="12">
-                    <v-btn
-                      color="default"
-                      small
-                      @click="initValues"
-                    >
+                    <v-btn color="default" @click="initValues">
                       Reset Values
                     </v-btn>
                   </v-col>
                 </v-row>
               </v-col>
-            </v-row>
-          </v-col>
-          <v-col cols="5">
-            <h4>
-              Expected Rewards
-            </h4>
-            <v-spacer />
-            <h5>
-              $
-              <span> {{rewardDay}} </span><br/>
-              <span class="date-opacity-7"> per day </span>
-            </h5>
-            <v-spacer />
-            <h5>
-              $
-              <span> {{rewardMonth}} </span><br/>
-              <span class="date-opacity-7"> per month </span>
-            </h5>
-            <v-spacer />
-            <h5>
-              $
-              <span> {{rewardYear}} </span><br/>
-              <span class="date-opacity-7"> per year </span>
-            </h5>
-          </v-col>
-        </v-row>
-        <v-row class="show-mobile">
-          <v-col cols="12">
-            <v-row>
-              <v-col cols="6" md="6" sm="6" xs="6">
-                <vuetify-money
-                  v-model="tradingFeeCollected"
-                  class="input-color-green"
-                  label="Trading fees collected"
-                  :outlined="false"
-                  :options="suffixUSD"
-                  :readonly="true"
-                />
-              </v-col>
-              <v-col cols="6" md="6" sm="6" xs="6" class="d-flex align-center">
-                <v-btn
-                  color="success"
-                >
+              <v-col
+                cols="6"
+                md="6"
+                sm="6"
+                xs="6"
+                class="d-flex align-center"
+              >
+                <v-btn color="success">
                   Sign Up Now
                 </v-btn>
               </v-col>
             </v-row>
           </v-col>
+          <v-col cols="6">
+            <h4>
+              Expected Rewards
+            </h4>
+            <v-btn-toggle v-model="rewardType">
+              <v-btn color="default" small>
+                KCS
+              </v-btn>
+              <v-btn color="default" small>
+                $
+              </v-btn>
+            </v-btn-toggle>
+            <v-spacer />
+            <h5>
+              <template v-if="rewardType === 1">
+                {{ rewardTypeString }}
+              </template>
+              <span> {{ rewardDay }} </span>
+              <template v-if="rewardType === 0">
+                {{ rewardTypeString }}
+              </template>
+              <br />
+              <span class="date-opacity-7"> per day </span>
+            </h5>
+            <v-spacer />
+            <h5>
+              <template v-if="rewardType === 1">
+                {{ rewardTypeString }}
+              </template>
+              <span> {{ rewardMonth }} </span>
+              <template v-if="rewardType === 0">
+                {{ rewardTypeString }}
+              </template>
+              <br />
+              <span class="date-opacity-7"> per month </span>
+            </h5>
+            <v-spacer />
+            <h5>
+              <template v-if="rewardType === 1">
+                {{ rewardTypeString }}
+              </template>
+              <span> {{ rewardYear }} </span>
+              <template v-if="rewardType === 0">
+                {{ rewardTypeString }}
+              </template>
+              <br />
+              <span class="date-opacity-7"> per year </span>
+            </h5>
+          </v-col>
         </v-row>
         <v-row class="show-mobile">
-          <v-col cols="12" class="show-mobile">
+          <v-col
+            cols="6"
+            md="6"
+            sm="6"
+            xs="6"
+            class="d-flex align-center"
+          >
             <v-row>
               <v-col cols="12">
-                <v-btn
-                  color="default"
-                  small
-                  @click="initValues"
-                >
+                <v-btn color="default" @click="initValues">
                   Reset Values
                 </v-btn>
               </v-col>
             </v-row>
+          </v-col>
+          <v-col
+            cols="6"
+            md="6"
+            sm="6"
+            xs="6"
+            class="d-flex align-center"
+          >
+            <v-btn color="success">
+              Sign Up Now
+            </v-btn>
           </v-col>
         </v-row>
       </div>
@@ -157,79 +180,86 @@
 </template>
 
 <script>
-  import axios from 'axios'
-  import dailyVolumeUrl from '@/utils/index'
+import axios from 'axios'
+import dailyVolumeUrl from '@/utils/index'
 
-  export default {
-    name: 'Calculator',
+export default {
+  name: 'Calculator',
 
-    data: () => ({
-      kcsStackedOption: {
-        suffix: "( KCS )",
-        precision: 0,
-      },
-      kcsOwnOption: {
-        suffix: "( KCS )",
-        precision: 0,
-      },
-      suffixPercent: {
-        suffix: "%",
-        precision: 3,
-      },
-      suffixUSD: {
-        prefix: "$",
-        precision: 0,
-      },
-      kcsStacked: 80118638,
-      kcsOwn: 10000,
-      dailyVolume: 1000000000,
-      averageTradingFee: 0.1,
-      apiVal: 0
-    }),
+  data: () => ({
+    kcsStackedOption: {
+      precision: 0,
+    },
+    kcsOwnOption: {
+      precision: 0,
+    },
+    suffixPercent: {
+      suffix: '%',
+      precision: 3,
+    },
+    suffixUSD: {
+      prefix: '$',
+      precision: 0,
+    },
+    kcsStacked: 80118638,
+    kcsOwn: 10000,
+    dailyVolume: 1000000000,
+    averageTradingFee: 0.1,
+    apiVal: 0,
+    rewardType: 0,
+  }),
 
-    computed: {
-      payoutPerDay() {
-        return 0.10 * this.dailyVolume / 100 / 2 / this.kcsStacked
-      },
-      
-      tradingFeeCollected() {
-        return this.dailyVolume / 100 * this.averageTradingFee
-      },
-
-      rewardDay() {
-        return parseFloat((this.payoutPerDay * this.kcsOwn * this.apiVal).toFixed(4))
-      },
-
-      rewardMonth() {
-        return parseFloat((this.rewardDay * 30).toFixed(4))
-      },
-
-      rewardYear() {
-        return parseFloat((this.rewardDay * 365).toFixed(2))
-      }
+  computed: {
+    payoutPerDay() {
+      return (0.1 * this.dailyVolume) / 100 / 2 / this.kcsStacked
     },
 
-    mounted() {
-      try {
-        axios.get(dailyVolumeUrl).then(res => {
-          if (res.status === 200) {
-            this.apiVal = res.data['kucoin-shares'].usd
-          }
-        })
-      } catch (e) {
-        console.log(e)
-      }
+    tradingFeeCollected() {
+      return (this.dailyVolume / 100) * this.averageTradingFee
     },
 
-    methods: {
-      initValues() {
-        this.kcsStacked = 80118638
-        this.kcsOwn = 10000
-        this.averageTradingFee = 0.1
-        this.dailyVolume = 1000000000
-      }
+    rewardDay() {
+      const dailyVal =
+        this.rewardType === 1
+          ? parseFloat(((this.payoutPerDay * this.kcsOwn * this.apiVal) / 7).toFixed(4))
+          : parseFloat(((this.payoutPerDay * this.kcsOwn) / 7.07).toFixed(4))
+      return dailyVal
+    },
+
+    rewardMonth() {
+      return parseFloat((this.rewardDay * 30).toFixed(4))
+    },
+
+    rewardYear() {
+      return parseFloat((this.rewardDay * 365).toFixed(2))
+    },
+
+    rewardTypeString() {
+      return this.rewardType === 0 ? 'KCS' : '$'
+    },
+  },
+
+  mounted() {
+    try {
+      axios.get(dailyVolumeUrl).then(res => {
+        if (res.status === 200) {
+          this.apiVal = res.data['kucoin-shares'].usd
+        }
+      })
+    } catch (e) {
+      console.log(e)
     }
-  }
+  },
+
+  methods: {
+    initValues() {
+      this.kcsStacked = 80118638
+      this.kcsOwn = 10000
+      this.averageTradingFee = 0.1
+      this.dailyVolume = 1000000000
+    },
+  },
+}
 </script>
 
 <style scopped>
@@ -275,12 +305,12 @@ label {
 h5 {
   margin-top: 1rem;
   font-size: 18px;
-  opacity: .8;
+  opacity: 0.8;
   font-weight: 500;
 }
 
 .date-opacity-7 {
-  opacity: .5;
+  opacity: 0.5;
   font-weight: 400;
   font-size: 18px;
   color: #000000de;
@@ -326,14 +356,14 @@ p {
 }
 .average-fee input {
   outline: none;
-  width: 100%;
+  width: 80%;
 }
 .average-fee span {
-    position: absolute;
-    top: 24px;
-    right: 0;
-    background: white;
-    width: 30px;
-    height: 25px;
+  position: absolute;
+  top: 24px;
+  right: 20%;
+  background: white;
+  width: 30px;
+  height: 25px;
 }
 </style>
